6fe4e17841e6b948ad280a8aa0d49c95861cc348
PalanikannanM <palanikannan.m2020@vitstudent.ac.in>
2023-02-05 19:53:07 +0000

    init template with docs, website and ui library

---
+++ apps/components/app/head.tsx
@@ -0,0 +1,7 @@
+export default function Head({ params }: { params: { slug: string } }) {
+  return (
+    <>
+      <title>Modern Full-Stack components to build your Next community powered by Rocket.Chat!</title>
+    </>
+  );
+}
